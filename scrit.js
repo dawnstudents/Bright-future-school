d262f14672d11647bc7830ff6cb934f12acbbf0b
dawnstudents <dawnacademy2833606@gmail.com>
2026-03-13 14:22:18 +0500

    workfull website of bright future school

---
+++ scrit.js
@@ -0,0 +1,26 @@
+// ===== Mobile Menu Toggle =====
+
+const menuToggle = document.getElementById("menuToggle");
+const navMenu = document.getElementById("navMenu");
+
+menuToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+});
+
+
+// ===== Smooth Scrolling =====
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+
+    anchor.addEventListener("click", function (e) {
+
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute("href"))
+            .scrollIntoView({
+                behavior: "smooth"
+            });
+
+    });
+
+});
